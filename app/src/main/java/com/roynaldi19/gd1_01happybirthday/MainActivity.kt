9ba7430628d04efd2a05be8a22b553e3e2364e94
roynaldi19 <roynaldi19@gmail.com>
2023-04-30 09:47:39 +0700

    update 23

--- app/src/main/java/com/roynaldi19/gd1_01happybirthday/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/gd1_01happybirthday/MainActivity.kt
@@ -2,7 +2,6 @@ package com.roynaldi19.gd1_01happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.roynaldi19.gd1_01happybirthday.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
